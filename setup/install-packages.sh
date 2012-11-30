4bd612cae07846ad342d5a59b61d60ba033cc31e
A <arexchi79@yahoo.com>
2012-11-29 18:15:55 -0800

    Add version numbers to setup install script so maintain consistency in versions across everyone

--- setup/install-packages.sh
+++ setup/install-packages.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+#
+# Make sure to include version so that we maintain consistency across all
+# working setups and production
+#
 
-pip install Flask
-pip install redis
-pip install py_bcrypt
-pip install Flask_SQLAlchemy
+pip install -I Flask==0.9
+pip install -I redis==2.7.1
+pip install -I py_bcrypt==0.2
+pip install -I Flask_SQLAlchemy==0.16
 easy_install -U distribute
 pip install -I MySQL_python==1.2.3
+pip install -I boto==2.6.0
+
+# $eof
