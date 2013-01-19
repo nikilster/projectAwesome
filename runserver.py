c2e601fc64fe83648a3e43d8bfd09c363e711a6e
A <arexchi79@yahoo.com>
2013-01-19 04:48:40 -0800

    Still doesn't work, think we need this

--- runserver.py
+++ runserver.py
@@ -8,5 +8,5 @@ if not os.path.exists(Constant.LOCAL_IMAGE_DIR):
 
 #Run
 port = int(os.environ.get('PORT', 5000))
-app.run(debug=app.config['DEBUG'], port=port)
+app.run(host='0.0.0.0', port=port, debug=app.config['DEBUG'])
 
